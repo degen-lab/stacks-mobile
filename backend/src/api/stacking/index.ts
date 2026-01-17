ee5fde9f50eb184537e9572c8f789e99cfdefd25
vndrei21 <stetco.andrei03@gmail.com>
2026-01-17 21:47:50 +0200

    feat(stacking): setup and implement routes for stacking data (#10)

---
+++ backend/src/api/stacking/index.ts
@@ -0,0 +1,14 @@
+import { FastifyInstance } from "fastify";
+import { StackingService } from "../../application/stacking/stackingService";
+import stackingPostRoutes from "./post";
+export default function stackingRoutes(app: FastifyInstance, {
+  stackingService
+} : {
+  stackingService: StackingService
+}) { 
+  app.register(
+    stackingPostRoutes, {
+      stackingService,
+    }
+  );
+}
---
+++ backend/src/api/validators/stackingValidator.ts
@@ -0,0 +1,8 @@
+import z from "zod";
+
+export const saveStackingDataSchema = z.object({
+  txId: z.string(),
+  poolName: z.string()
+    .min(3)
+    .max(40),
+});
