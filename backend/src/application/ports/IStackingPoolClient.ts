c1300d8e0e6c7287cb83de0ff4ed4341b4df9627
vndrei21 <stetco.andrei03@gmail.com>
2026-01-23 21:40:32 +0200

    fix(stacking): fixed reward computation method to compute the rewarded amount from starting cycle to end cycle (#10)

--- backend/src/application/ports/IStackingPoolClient.ts
+++ backend/src/application/ports/IStackingPoolClient.ts
@@ -1,4 +1,4 @@
 
 export interface IStackingPoolClient {
-  delegationTotalRewards(address: string): Promise<number>;
+  delegationTotalRewards(address: string, startCycleId: number, endCycleId: number): Promise<number>;
 }
--- backend/src/infra/stacks/fastPoolClient.ts
+++ backend/src/infra/stacks/fastPoolClient.ts
@@ -2,7 +2,7 @@ import { IStackingPoolClient } from "../../application/ports/IStackingPoolClient
 import { FAST_POOL_STACKING_DATA_JSON_URL } from "../../shared/constants";
 
 export class FastPoolClient implements IStackingPoolClient {
-    async delegationTotalRewards(address: string): Promise<number> {
+    async delegationTotalRewards(address: string, startCycleId: number, endCycleId: number): Promise<number> {
       const res = await fetch(FAST_POOL_STACKING_DATA_JSON_URL(address), {
         headers: {
           "Accept": "application/vnd.github+json",
@@ -18,7 +18,13 @@ export class FastPoolClient implements IStackingPoolClient {
       const encodedJsonContent = data.content;
       const decodedContent =  Buffer.from(encodedJsonContent, 'base64').toString();
       const jsonStackingData = JSON.parse(decodedContent);
-      
-      return jsonStackingData.totalRewards;
+      let totalStackingRewards: number = 0;
+      for(let i = startCycleId; i < endCycleId; i += 1) {
+        if (!jsonStackingData.cycles[`${i}`]) {
+          break;
+        }
+        totalStackingRewards += jsonStackingData.cycles[`${i}`].rewards;
+      }
+      return totalStackingRewards;
     }
 }
